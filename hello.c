3f6fd6c29dad55754b33a35ca4bf781e52a343cf
hhhxxx111 <675521948@qq.com>
2024-11-27 22:24:59 +0800

    第一次提交

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+
+    int n = 10;
+    while (n--)
+    {
+        printf("hello \n");
+    }
+    
+    int a = 1,b = 2;
+    int c = 10;
+    printf("hello----%d %d\n",a+b,c);
+    
+    return 1;
+}
